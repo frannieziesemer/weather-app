a85aaf60dbb73c92bdaf84e58127a12928e65451
frannieziesemer <52860762+frannieziesemer@users.noreply.github.com>
2019-12-19 21:25:08 +0100

    JS forecast api call for current location

--- script.js
+++ script.js
@@ -122,6 +122,9 @@ function searchLocation(position) {
   let apiKey = "498800558a7c5bd6e9bc3ec1ccb9adfe";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function retreiveCurrentLocation(event) {
